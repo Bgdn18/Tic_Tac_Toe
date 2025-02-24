cba74c9f53f898f1902cc0f30163c54027d223b0
Bogdan Khorolsky <krati18012012@gmail.com>
2025-02-24 14:04:58 -0800

    Add project files.

---
+++ Tic Tac Toe/Tic_Tac_Toe.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Tic_Tac_Toe
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tic_Tac_Toe));
+            SuspendLayout();
+            // 
+            // Tic_Tac_Toe
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(799, 611);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Tic_Tac_Toe";
+            Text = "Tic Tac Toe!";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Tic Tac Toe/Tic_Tac_Toe.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Tic_Tac_Toe : Form
+    {
+        List<Button> buttons = new List<Button>();
+        string tag;
+        public Tic_Tac_Toe()
+        {
+            InitializeComponent();
+            Init();
+   /* */        // Запрет изменения размера окна
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false; // Убираем кнопку максимизации 
+        }
+
+        bool CheckWin()
+        {
+            for (int i = 0; i < 9; i += 3)
+            {
+                if (buttons[i].Text == buttons[i + 1].Text && buttons[i].Text == buttons[i + 2].Text && buttons[i].Text != "")
+                {
+                    return true;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (buttons[i].Text == buttons[i + 3].Text && buttons[i].Text == buttons[i + 6].Text && buttons[i].Text != "")
+                {
+                    return true;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (buttons[0].Text == buttons[4].Text && buttons[i].Text == buttons[8].Text && buttons[4].Text != "")
+                {
+                    return true;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (buttons[2].Text == buttons[4].Text && buttons[i].Text == buttons[6].Text && buttons[4].Text != "")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        void GenerateTag()
+        {
+            var r = new Random();
+            tag = r.Next(0, 2) != 1 ? "X" : "O";
+        }
+        void ButtonsInit()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons.Add(new Button
+                    {
+                        Size = new Size(200, 200),
+                        Location = new Point(i * 200, j * 200),
+                        TabStop = false,
+                        Font = new Font ("Arial", 70),
+                    });
+                    buttons[buttons.Count - 1].Click += ButtonClick;
+                    Controls.Add(buttons[buttons.Count - 1]);
+                }
+            }
+        }
+
+        private void ButtonClick(object? sender, EventArgs e)
+        {
+            var currentButton = sender as Button;
+            currentButton.Text = tag;
+            currentButton.Click -= ButtonClick;
+            if (CheckWin())
+            {
+                MessageBox.Show("won " + tag);
+                foreach (Button item in buttons)
+                {
+                    item.Click -= ButtonClick;
+                }
+            }
+
+             /* currentButton.Enabled = false;
+             делает кнопку полупрозрачной (1 вариант)*/
+
+             tag = tag == "X" ? "O" : "X";
+        }
+
+        void Init()
+        {
+           // ClientSize = new Size(600, 600);
+            ButtonsInit();
+            GenerateTag();
+        }
+
+    }
+
+}
